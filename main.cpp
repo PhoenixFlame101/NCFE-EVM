416080aadd23cc9e3d197f0db231f856a023429e
PhoenixFlame101 <51199011+PhoenixFlame101@users.noreply.github.com>
2019-05-31 20:52:20 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,40 +4,40 @@ using namespace std;
 
 int main () {
     
-	int vote;
-	int vf1, vf2, vf3;
+	int vote;	    	// vote is the vote cast by the user
+	int c1, c2, c3;		// c1, c2 and c3 are the candidates
 
-    cout << "Enter a number from 1 to 3";
-    cin >> vote;
+    cout << "Enter a number from 1 to 3";		// user votes for either candidate 1, 2 or 3
+    cin >> vote;								// vote is accounted for
 
     if(vote = 1)
     {
-        vf1++;
+        c1++;		// candidate 1 gets the vote
     }
 
     else if(vote = 2)
     {
-        vf2++;
+        c2++;		// candidate 2 gets the vote
     }
 
     else if(vote = 3)
     {
-        vf3++;
+        c3++;		// candidate 3 gets the vote
     }
     
-    if(vf1 > vf2 && vf1 > vf3)
+    if(c1 > c2 && c1 > c3)			// candidate 1 gets the most votes
     {
-    	cout << "Cadidate 1";
+    	cout << "Cadidate 1 has won the election";
     }
 
-    else if(vf2 > vf1 && vf2 > vf3)
+    else if(c2 > c1 && c2 > c3)		// candidate 2 gets the most votes
     {
-    	cout << "Cadidate 2";
+    	cout << "Cadidate 2 has won the election";
     }
 
-    else if(vf3 > vf2 && vf3 > vf1)
+    else if(c3 > c1 && c3 > c2)		// candidate 3 gets the most votes
     {
-    	cout << "Cadidate 3";
+    	cout << "Cadidate 3 has won the election";
     }
 
 }
