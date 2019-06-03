3c45e73d973b701637b904a80312c3dcf52609df
PhoenixFlame101 <51199011+PhoenixFlame101@users.noreply.github.com>
2019-06-03 19:22:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-# 
 
-vote = int(input("Enter first vote: "))
+#    This is the main file that controls the backend.
+
+vote = int(input("Enter vote: ")) # vote is cast by the user
+
+c1 = 0
+c2 = 0
+c3 = 0
 
 if (vote == 1):
 	c1 += 1
@@ -8,12 +13,34 @@ elif (vote == 2):
 	c2 += 1
 elif (vote == 3):
 	c3 += 1
+else:
+	print('Error: Invalid Vote.  Please Try Again.')
 
-def voteC1(vote)
+# if (c1 != 0) and (c2 != 0) and (c3 != 0):
+if (c1 == c2) and (c1 != c3):
+	print('Candidate 1 and 2 are tied with', c1, 'votes')
+	print('Candidate 3 has got', c3, 'votes')
 
-if (c1 > c2) and (c1 > c3):
+elif (c2 == c3) and (c2 != c1):
+	print('Candidate 2 and 3 are tied with', c2, 'votes')
+	print('Candidate 1 has got', c1, 'votes')
+
+elif (c1 == c2) and (c2 == c3):
+	print('Candidate 1 and 2 and 3 are tied with', c1, 'votes')	
+
+elif (c1 > c2) and (c1 > c3):
 	print('Candidate 1 has won the election with', c1, 'votes')
 	print('Candidate 2 has got', c2, 'votes')
 	print('Candidate 3 has got', c3, 'votes')
+
+elif (c2 > c1) and (c2 > c3):
+	print('Candidate 2 has won the election with', c2, 'votes')
+	print('Candidate 1 has got', c1, 'votes')
+	print('Candidate 3 has got', c3, 'votes')
+
+elif (c2 > c1) and (c2 > c3):
+	print('Candidate 3 has won the election with', c3, 'votes')
+	print('Candidate 1 has got', c1, 'votes')
+	print('Candidate 2 has got', c2, 'votes')
 
 input('Press ENTER to exit')
