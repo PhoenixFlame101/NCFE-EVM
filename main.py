20d806b6644bbba0ce1cf84dfd2bbab48516eb8d
PhoenixFlame101 <51199011+PhoenixFlame101@users.noreply.github.com>
2020-05-05 21:47:59 +0530

    Changed folder structure

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # This is the program meant to run during the voting session
 
-
 import admin_controls
 import database_linker
 import gui
 
 # We get the votes casts from the GUI after every session
 # The dict is in the form {[post_name, candidate_voted_for]}
-# candidates = gui.main()
+candidates = gui.start()
 
 # We add the votes received per candidate to the votes of the resp. candidate in MongoDB
 # database_linker.add_votes_to_db(candidates)
