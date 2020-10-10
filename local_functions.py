06027db4416f211ac3076cd8b6002fbbfec2eed9
PhoenixFlame101 <abhinavchris@gmail.com>
2020-10-10 11:04:47 +0530

    Several code improvements
    Converted tab indentation to spaces; Added comments; ran code through pylint; Updated docstrings

--- local_functions.py
+++ local_functions.py
@@ -1,72 +1,94 @@
-# This module contains functions that are device specific
+""" This module contains functions that manipulate local_var.encrypted and resizes images"""
 
-from PIL import Image, UnidentifiedImageError
 from os import listdir, rename
-
-def resize_image(picture_path):
-	try:
-		pic = Image.open(picture_path)
-		aspect_ratio = pic.size[1]/pic.size[0]
-		if aspect_ratio >= 1:
-			pic = pic.resize((300, int(300*aspect_ratio)))
-			pic = pic.crop((0, (pic.size[1]-300)/2, 300, pic.size[1]-(pic.size[1]-300)/2))
-			pic.save(picture_path)
-		elif aspect_ratio < 1:
-			aspect_ratio = pic.size[0]/pic.size[1]
-			pic = pic.resize((int(300*aspect_ratio), 300))
-			pic = pic.crop(((pic.size[0]-300)/2, 0, pic.size[0]-(pic.size[0]-300)/2, 300))
-			pic.save(picture_path)
-	except UnidentifiedImageError:
-		image_name = picture_path[len(picture_path)-picture_path[::-1].index('/'):]
-		if not image_name.startswith('[Corrupt]'):
-			rename(picture_path, picture_path[:len(picture_path)-picture_path[::-1].index('/')-1]+'/[Corrupt] '+image_name)
-
+from PIL import Image, UnidentifiedImageError
 
 def resize_images_in_folder(path):
-	for image in listdir(path):
-		resize_image(path+'/'+image)
+    """ This function is run at the start of the program.
+        It crops and resizes images in candidate_photos to 300x300 px """
+
+    # Iterates through all images in the given folder
+    for image in listdir(path):
+        picture_path = path+'/'+image
+
+        try:
+            # Resizes and crops images depending on the orientation of the image
+            pic = Image.open(picture_path)
+            aspect_ratio = pic.size[1]/pic.size[0]
+
+            if aspect_ratio >= 1: # If image is in portrait/square orientation
+                pic = pic.resize((300, int(300*aspect_ratio)))
+                pic = pic.crop((0, (pic.size[1]-300)/2, 300, pic.size[1]-(pic.size[1]-300)/2))
+                pic.save(picture_path)
+
+            elif aspect_ratio < 1: # If the image in landscape orientation
+                aspect_ratio = pic.size[0]/pic.size[1]
+                pic = pic.resize((int(300*aspect_ratio), 300))
+                pic = pic.crop(((pic.size[0]-300)/2, 0, pic.size[0]-(pic.size[0]-300)/2, 300))
+                pic.save(picture_path)
+
+        except UnidentifiedImageError:
+            # Run if the file is not an image file
+            # Places '[Corrupt] at the beginning of the file name 
+            image_name = picture_path[len(picture_path)-picture_path[::-1].index('/'):]
+            if not image_name.startswith('[Corrupt]'):
+                rename(picture_path, picture_path[:len(picture_path)-picture_path[::-1]  \
+                .index('/')-1]+'/[Corrupt] '+image_name)
 
 
 def store_house_choice(choice):
-	with open('local_vars.encrypted', 'r+') as f:
-		try:
-				uri = f.readlines()[-1]
-		except:
-				uri = ''
-		f.truncate(0)
-		f.seek(0)
-		f.writelines([f'House Choice: {choice.lower()}'+'\n', uri])
+    """ Adds house choice to the file local_vars.encrypted """
+
+    with open('local_vars.encrypted', 'r+') as file:
+
+        # Attempts to retreive db uri; default is blank
+        try:
+            uri = file.readlines()[-1]
+        except IndexError:
+            uri = 'DB URI: '
+        
+        # File is overwritten with the given house choice and db uri
+        file.truncate(0)
+        file.seek(0)
+        file.writelines([f'House Choice: {choice.lower()}'+'\n', uri])
 
 
 def get_house_choice():
-	try:
-		with open('local_vars.encrypted', 'r+') as f:
-			return f.readlines()[0][14:-1]
-	except FileNotFoundError:
-		store_house_choice('kingfisher')
-		return get_house_choice()
+    """ Retrieves the house choice from local_vars.encrypted """
+
+    try:
+        with open('local_vars.encrypted', 'r+') as file:
+            return file.readlines()[0][14:-1]
+    except FileNotFoundError:
+        store_house_choice('kingfisher')
+        return get_house_choice()
 
 
 def store_db_uri(uri):
-	try:
-		with open('local_vars.encrypted', 'r+') as f:
-			house_choice = f.readlines()[0]
-			f.truncate(0)
-			f.seek(0)
-			f.writelines([house_choice, f'DB URI: {uri}'])
-	except:
-		with open('local_vars.encrypted', 'w+') as f:
-			f.writelines(['\n', f'DB URI: {uri}'])
+    """ Adds database connection string to the file local_vars.encrypted """
+
+    try:
+        with open('local_vars.encrypted', 'r+') as file:
+            house_choice = file.readlines()[0]
+            file.truncate(0)
+            file.seek(0)
+            file.writelines([house_choice, f'DB URI: {uri}'])
+    except (IndexError, FileNotFoundError):
+        with open('local_vars.encrypted', 'w+') as file:
+            file.writelines(['House Choice: ', f'DB URI: {uri}'])
 
 
 def get_db_uri():
-	try:
-		with open('local_vars.encrypted', 'r+') as f:
-			uri = f.readlines()[1][8:]
-			if '/' not in uri:
-				return 'mongodb://localhost:27017/'
-			else:
-				return uri
-	except FileNotFoundError:
-		store_db_uri('')
-		return get_db_uri()
+    """ Retrieves the db uri from local_vars.encrypted """
+
+    try:
+        with open('local_vars.encrypted', 'r+') as file:
+            try:
+                uri = file.readlines()[1][8:]
+            except IndexError:
+                return 'mongodb://localhost:27017/'
+            if '/' not in uri:
+                return 'mongodb://localhost:27017/'
+            return uri
+    except FileNotFoundError:
+        return 'mongodb://localhost:27017/'
--- sec_code.py
+++ sec_code.py
@@ -1,97 +1,99 @@
-# This program generates and check the security codes
+""" This module generates, encrypts, stores and retreives security codes and the admin password  """
 
-from cryptography.fernet import Fernet
 import random
 import string
+from cryptography.fernet import Fernet
 from fpdf import FPDF
 import database_linker
 
 
-# A predetermined encryption key
+# A predetermined key for symmetric AES-128 enctryption
 key = Fernet(b'hdQqJb2hS5g07COO1nOasMHhhn_hmDdwPCyHvGSH57c=')
 
 
 def pass_set(_pass):
-	""" Sets the admin password to the string provided as an arguement
-		No password is set by default """
-	database_linker.add_password_to_db(key.encrypt(_pass.encode('utf-8')))
+    """ Sets the admin password to the string provided as an arguement
+        123 is set by default """
+
+    # Encrypts the bytes string of _pass and stores it in the db
+    database_linker.add_password_to_db(key.encrypt(_pass.encode('utf-8')))
 
 
 def pass_is_valid(_pass):
-	""" Checks if the entered password is the admin password or not """
+    """ Checks if the entered password is the admin password or not """
 
-	# Takes the password from the MongoDB database
-	password = database_linker.get_password_from_db()
-
-	if _pass == key.decrypt(password).decode('utf-8'):
-		return True
-	else:
-		return False
+    # Takes the password from the MongoDB database
+    password = database_linker.get_password_from_db()
+    return _pass == key.decrypt(password).decode('utf-8')
 
 
 def code_gen(num_of_codes):
-	""" Generates 1936 unique codes and encrypts and stores them in a text file
-		Run before the voting begins """
-	codes = set()
+    """ Generates 1936 unique codes and encrypts and stores them in a text file
+        Run before the voting begins """
 
-	# Generates codes and adds them to a set 'codes'
-	while len(codes) != num_of_codes:
-		codes.add(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(5)))
-	plaintext_codes = codes
+    # Generates random 5 character alphanumeric codes and adds them to a set 'codes'
+    plaintext_codes = set()
+    while len(plaintext_codes) != num_of_codes:
+        plaintext_codes.add(''.join(random.choice(
+            string.ascii_uppercase + string.digits) for _ in range(5)))
 
-	# Encrypts the codes
-	codes = list(map(lambda x: key.encrypt(x.encode('utf-8')), list(codes)))
+    # Encrypts the codes
+    encrypted_codes = list(map(lambda x: key.encrypt(
+        x.encode('utf-8')), list(plaintext_codes)))
 
-	# Adds the votes to the db
-	database_linker.add_codes_to_db(codes)
-	database_linker.add_codes_to_used([])
+    # Adds the encrypted codes to the db, along with an empty array for used codes
+    database_linker.add_codes_to_db(encrypted_codes)
+    database_linker.add_codes_to_used([])
 
-	return plaintext_codes
+    # Returns unencrypted codes which are then printed as codes.pdf
+    return plaintext_codes
 
 def code_is_valid(code):
-	""" Checks if the code entered is valid
-		Run before voting to initiate the program """
-	code = code.upper()
+    """ Checks if the code entered is valid
+        Run before voting to initiate the program """
 
-	# Gets the codes from the db
-	codes = database_linker.get_codes_from_db()
+    # Fetches and decrypts the codes from the db
+    codes = database_linker.get_codes_from_db()
+    codes = list(map(lambda x: key.decrypt(x).decode('utf-8'), list(codes)))
+    code = code.upper()
 
-	# Decrypts the codes into plaintext
-	codes = list(map(lambda x: key.decrypt(x).decode('utf-8'), list(codes)))
+    # Check the provided code against the codes in the db
+    if code in set(codes):
+        # Removes the entered codes from the file so it cannot be reused
+        codes.remove(code)
 
-	if code in set(codes):
-		# Removes the entered codes from the file so it cannot be reused
-		codes.remove(code)
-		codes = list(map(lambda x: key.encrypt(x.encode('utf-8')), list(codes)))
-		database_linker.add_codes_to_db(codes)
-		database_linker.add_codes_to_used(database_linker.get_used_codes()+[key.encrypt(code.encode('utf-8'))])
-		return True
-	else:
-		used_codes = list(map(lambda x: key.decrypt(x).decode('utf-8'), database_linker.get_used_codes()))
-		return ('Already Used' if code in used_codes else 'Invalid')
+        # Reencrypts the codes and adds them back to the db
+        codes = list(map(lambda x: key.encrypt(x.encode('utf-8')), list(codes)))
+        database_linker.add_codes_to_db(codes)
+
+        # Adds the used codes to the 'used_codes' collection in the db
+        database_linker.add_codes_to_used(
+            database_linker.get_used_codes()+[key.encrypt(code.encode('utf-8'))])
+        return True
+
+    # Determines the error code depending on whether or not the code was previously used
+    used_codes = list(
+        map(lambda x: key.decrypt(x).decode('utf-8'), database_linker.get_used_codes()))
+    return 'Already Used' if code in used_codes else 'Invalid'
 
 
-def split(arr, n):
-	""" Splits a list into a 2D array of lists of n elements each """
-	for i in range(0, len(arr), n):
-		yield arr[i : i+n]
+def split(arr, num):
+    """ Splits a list into a 2D array of lists of n elements each """
+    for i in range(0, len(arr), num):
+        yield arr[i : i+num]
 
 
 def code_print(*args):
-	""" Generates a PDF of codes """
-	num_of_codes = 1936 if args == () else (484*args[0])
-	codes = list(split(list(code_gen(num_of_codes)), 11))  # 2D array of 11 codes in each list
+    """ Generates a PDF of codes """
+    num_of_codes = 1936 if args == () else (484*args[0])
+    codes = list(split(list(code_gen(num_of_codes)), 11))  # 2D array of 11 codes in each list
 
-	# Writes the codes to a PDF file
-	pdf = FPDF()
-	pdf.add_page()
-	pdf.set_font('Courier', size=12)
-	for row in codes:
-		for code in row:
-			pdf.cell(17, 6, txt=code, border=True, ln=0, align='C')
-		pdf.ln()
-	pdf.output('codes.pdf')
-
-
-# pass_set('123')
-# code_print()
+    # Writes the codes to a PDF file
+    pdf = FPDF()
+    pdf.add_page()
+    pdf.set_font('Courier', size=12)
+    for row in codes:
+        for code in row:
+            pdf.cell(17, 6, txt=code, border=True, ln=0, align='C')
+        pdf.ln()
+    pdf.output('codes.pdf')
